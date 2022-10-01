eca85be46b3d07562c8e34c098ac0c905a61b691
Jonas Hall <41904024+hallfjonas@users.noreply.github.com>
2022-09-30 23:56:50 -0400

    update colors etc

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+
+    private Camera _mainCamera;
+
+    private Vector3 _viewOffset = new Vector3(3.0f, 3.0f, 3.0f);
+
+    private Vector3 _oldPlayerPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _mainCamera = Camera.main;
+
+        _mainCamera.transform.position = transform.position + _viewOffset;
+        _oldPlayerPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Update the camera
+        UpdateCamera();
+
+        // Update the clicked tile
+        GetPathFromClick();
+
+        // Keep track of old player position
+        _oldPlayerPosition = transform.position;
+    }
+
+    /// <summary>
+    /// Update the camera position according to player motion
+    /// </summary>
+    void UpdateCamera()
+    {
+        _mainCamera.transform.position += transform.position - _oldPlayerPosition;
+        _mainCamera.transform.LookAt(transform);
+    }
+
+    void GetPathFromClick()
+    {
     }
 }
