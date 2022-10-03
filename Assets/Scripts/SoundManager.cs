a443d8a79f72ab95b49ca3b01a80af013acd41c7
Jonas Hall <41904024+hallfjonas@users.noreply.github.com>
2022-10-03 17:08:33 -0400

    new soundeffects

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    public AudioSource disableBoos;
+    public AudioSource disableBoss;
     public AudioSource pitfallSound;
+    public AudioSource mainBackgroundMusic;
+    public AudioSource bossOneBackgroundMusic;
+    public AudioSource bossTwoBackgroundMusic;
 
     private GameObject player;
     private PitfallManager pitfallManager;
     private PlayerManager playerManager;
+
+    private int bossFight = 0;
 
     private void Awake()
     {
@@ -17,25 +22,53 @@ public class SoundManager : MonoBehaviour
         pitfallManager = player.GetComponent<PitfallManager>();
         playerManager = player.GetComponent<PlayerManager>();
 
-        disableBoos.volume = SoundConstants.VOLUME;
+        disableBoss.volume = SoundConstants.VOLUME;
         pitfallSound.volume = SoundConstants.VOLUME;
+        mainBackgroundMusic.volume = SoundConstants.VOLUME;
+        bossOneBackgroundMusic.volume = SoundConstants.VOLUME;
+        bossTwoBackgroundMusic.volume = SoundConstants.VOLUME;
     }
 
     // Start is called before the first frame update
     void Start()
     {
         OverworldManager.instance.OnBossDisable += PlayDisableSound;
+        BattleTimeManager.instance.OnBattleStart += PlayBossFightMusic;
+        BattleTimeManager.instance.OnBattleEnd += PlayBackgroundMusic;
         pitfallManager.OnPitfallStateChange += PlayPickUpSound;
         playerManager.OnUnlockPitfall += PlayPickUpSound;
     }
 
     public void PlayDisableSound(System.Object src, System.EventArgs e)
     {
-        disableBoos.Play();
+        disableBoss.Play();
     }
 
     public void PlayPickUpSound(System.Object src, System.EventArgs e)
     {
         pitfallSound.Play();
+    }
+
+    public void PlayBossFightMusic(System.Object src, System.EventArgs e)
+    {
+        if (bossFight == 0)
+        {
+            mainBackgroundMusic.Stop();
+            bossOneBackgroundMusic.Play();
+        } else
+        {
+            mainBackgroundMusic.Stop();
+            bossTwoBackgroundMusic.Play();
+        }
+
+        bossFight += 1;
+    }
+
+
+    public void PlayBackgroundMusic(System.Object src, System.EventArgs e)
+    {
+        bossOneBackgroundMusic.Stop();
+        bossTwoBackgroundMusic.Stop();
+        mainBackgroundMusic.Play();
     }
 }
