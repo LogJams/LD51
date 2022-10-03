ed68f777d30fc19b6556fa66015ad787d24ed7dc
Logan <Logiant@users.noreply.github.com>
2022-10-03 18:29:23 -0400

    Final changes, it's submission hour!

--- Assets/Scripts/SoundConstants.cs
+++ Assets/Scripts/SoundConstants.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public static class SoundConstants
 {
-    public static float VOLUME = 0.01f;
-    public static float MUSICVOLUME = 0.05f;
+    public static float VOLUME = 0.01f * 5f;
+    public static float MUSICVOLUME = 0.05f * 5f;
 }
